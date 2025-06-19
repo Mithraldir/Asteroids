5ba2c7873d318a6725efed880b307897c2f5e27b
Mithraldir <frodo.baggins.junior@gmail.com>
2025-06-18 20:44:08 -0400

    Added player character and made it turn

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from constants import *
+from player import Player
 
 import pygame
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        player.update(dt)
+        
+    
 
 
 if __name__ == "__main__":
